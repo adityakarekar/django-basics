7e209ba6563c1ad51a30b1eab825c3d5d82b5421
Aditya <adityakarekar0311@gmail.com>
2026-02-23 23:21:20 +0530

    added content

--- myProject6/blog/views.py
+++ myProject6/blog/views.py
@@ -9,9 +9,11 @@ def blog_details(request):
     post={
         "title":"My #2 Templates Post",
         "description":"Dummy descrription",
-        "author":None,
+        "author":"no",
         "created_at":datetime(2025,8,19,10,3),
         "comments_count":5,
+        "price":100,
+        "number":2,
         "tags":["Django","Python"]
     }
     return render(request,"blog/blog_details.html",{"post":post})
